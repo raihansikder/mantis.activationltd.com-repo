1223c17711b99ba32c666a557b540d28a756daf9
raihansikder <raihan.act@gmail.com>
2013-03-30 23:33:11 +0600

    do not change status on assign

--- config_inc.php
+++ config_inc.php
@@ -6,4 +6,12 @@
 	$g_db_password = 'HJel2h4XuU9E3E0H';
 	$g_file_upload_method	= DISK;
 	$g_absolute_path_default_upload_folder ='uploads/';
+	
+	/**
+	 * Automatically set status to ASSIGNED whenever a bug is assigned to a person.
+	 * This is useful for installations where assigned status is to be used when
+	 * the bug is in progress, rather than just put in a person's queue.
+	 * @global int $g_auto_set_status_to_assigned
+	 */
+	$g_auto_set_status_to_assigned	= OFF;
 ?>
